83e7889b0db6c8aab5532130d052a8289cafd9be
SHOHET, MICHAEL <mlshohset@gmail.com>
2023-01-13 09:52:18 -0500

    add feature files and step def

---
+++ step-definitions/steps.js
@@ -0,0 +1,27 @@
+import  { Given, When, Then } from "@cucumber/cucumber";
+
+Given("the user is on login page", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+When(
+  "the user enters username as {string} and password as {string}",
+  function (username, password) {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
+
+When("clicks on login button", function () {
+  // Write code here that turns the phrase above into concrete actions
+  return "pending";
+});
+
+Then(
+  "the user must navigate to secure area page displaying a message {string}",
+  function (successMessage) {
+    // Write code here that turns the phrase above into concrete actions
+    return "pending";
+  }
+);
